b221720597a13da016cc83b08c533b3794e56c10
Ian Thompson <ianthompson@nicelion.com>
2018-02-01 10:27:02 -0500

    added some stuff

--- EnhancedForLoop.java
+++ EnhancedForLoop.java
@@ -2,6 +2,49 @@ import java.util.ArrayList;
 
 public class EnhancedForLoop
 {
+    public static double mean(double nums[]) {
+        int sum = 0;
+        
+        for (double x: nums) {
+            sum += x;
+        } 
+        
+        return sum / nums.length;
+    }
+    
+    public static double max(double nums[]) {
+        double max = nums[0];
+        
+        for (double x: nums) {
+            if (x > max) {
+                max = x;
+            }
+        }
+        
+        return max;
+    }
+    
+    public static int sumEvenVals(int nums[]) {
+        int sum = 0;
+        
+        for (int x: nums) {
+            if (x % 2 == 0)
+                sum += x;
+            }
+        
+        return sum;
+    }
+    
+    public static int vowelCount(String phrase) {
+        int count = 0;
+        
+        for (String s: phrase){
+        }
+        
+        return count;
+    }
+    
+    
     public static void main(String[] args)
     {
         /********** Examples **********/
@@ -50,29 +93,32 @@ public class EnhancedForLoop
             morePets.remove(i);
             p.eat();
         }
+        System.out.println();
         /********** Your turn **********/
         
         // #1 - Write a method mean(double nums[]) which uses an enhanced
         //      for loop to return the mean of the elements in nums. Test
         //      the method here.
         
+        double[] dubArr = {3.1, 3.3, 6.5, 3.12, 98.4, 98.3, 98.5};
+        System.out.println(mean(dubArr));
         
         
-
+        System.out.println();
         // #2 - Write a method max(double nums[]) which uses an enhanced
         //      for loop to return the maximum value of the elements in
         //      nums. Then test the method.
-
-
-
         
+        System.out.println(max(dubArr));
+        
+        System.out.println();
         // #3 - Write a method sumEvenVals that uses an enhanced for 
         //      loop to return the sum of all even values stored in an 
         //      Array.  Then test the method.
+        
+        System.out.println(sumEvenVals(arr));
 
-
-
-
+        System.out.println();
         // #4 - Write a method vowelCount that uses an enhanced for loop
         //      to count the number of vowels (a,e,o,u only) in a list
         //      of words.
