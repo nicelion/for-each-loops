730224053eca4443846623ec92168207c0bb02e8
Ian Thompson <ianthompson@nicelion.com>
2018-02-02 10:09:01 -0500

    finished

--- EnhancedForLoop.java
+++ EnhancedForLoop.java
@@ -27,6 +27,8 @@ public class EnhancedForLoop
     public static int sumEvenVals(int nums[]) {
         int sum = 0;
         
+
+        
         for (int x: nums) {
             if (x % 2 == 0)
                 sum += x;
@@ -37,13 +39,44 @@ public class EnhancedForLoop
     
     public static int vowelCount(String phrase) {
         int count = 0;
-        
-        for (String s: phrase){
+        char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+        for (char c: phrase.toCharArray()){
+            for (char v: vowels) {
+                if (c == v){
+                    count++;
+                }
+            }
         }
         
         return count;
     }
     
+    public static int sumEvenLocs(int[] nums) {
+        int sum = 0;
+        int index = 0;
+        for (int n: nums) {
+            if (index % 2 == 0){
+                index++;
+                sum += n;
+                
+            }
+            
+        }
+        
+        return sum;
+    }
+    
+    public static int sumEvenLocsFor(int[] nums) {
+        int sum = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 2 == 0) {
+                sum += nums[i];
+            }
+        }
+        
+        return sum;
+    }
     
     public static void main(String[] args)
     {
@@ -123,20 +156,24 @@ public class EnhancedForLoop
         //      to count the number of vowels (a,e,o,u only) in a list
         //      of words.
         
+        System.out.println(vowelCount("the lazy dog jumped over the quick brown fox"));
         
-        
-
+        System.out.println();
         // #5 - Write a method sumEvenLocs that uses an enhanced for 
         //      loop to find the sum of all values stored with even 
         //      numbered indices in an Array. Then test the method.
         
+        System.out.println(sumEvenLocs(arr));
         
-        
-        
+        System.out.println();
         // #6 - Solve #5 using a regular for loop. Then explain why a
         //      regular for loop is more appropriate for that problem.
+        System.out.println(sumEvenLocsFor(arr));
         
-        
+        /*
+         * It is more appropriate to use a standart for loop because you can access the index without having to define it before starting 
+         * the for loop.
+         */
         
     }
 }
